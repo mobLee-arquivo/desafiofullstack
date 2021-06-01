0ef55ef22171a85a33d3965942fe385065fc27a0
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-31 23:47:05 -0300

    feat(api): apollo server to be a graphql endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { QuestionsResolver } from './graphql/resolvers/question';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer, ExpressContext } from 'apollo-server-express/dist/ApolloServer';
 import { port } from './common/consts';
 
 async function main() {
@@ -7,9 +10,20 @@ async function main() {
   app.use(express.json());
   app.disable('X-Powered-By');
 
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [QuestionsResolver],
+    }),
+    context: ({ req, res }: ExpressContext) => ({ res, req }),
+    introspection: true,
+    playground: true,
+  });
+
   app.listen(port, () => {
     console.log(`\n🚀 Servidor iniciado com sucesso na porta ${port}`);
   });
+
+  apolloServer.applyMiddleware({ app });
 }
 
 main().catch((e) => console.error(e));
